5f3109782f4177f5eb3cda4aad10c47bada73ef0
meghanctucker <meghanctucker@gmail.com>
2020-08-24 18:22:19 -0400

    finishing programming and basics
    finished majority of programming including javascript and basic styling with mobile first. Must add toggle switches, inline SVGs, add detailed styling (fonts, padding margins), toggle switches and extra credit

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,17 @@
+//GRAPH OBJECTS
 const lineChart = document.getElementById('traffic');
 const barChart = document.getElementById('dailyTraffic');
 const donutChart = document.getElementById('mobileUsers');
 
+//ALERT BANNER OBJECTS
+const alertBanner = document.getElementById('alert');
+
+//MESSAGE FIELD OBJECTS
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+//GRAPH CODE
 const Traffic = new Chart(lineChart, {
   type: 'line',
   data: {
@@ -45,5 +55,36 @@ const Mobile = new Chart(donutChart, {
         data: [431, 765, 1256]
       }
     ]
+  }
+});
+
+
+//ALERT BANNER CODE
+alertBanner.innerHTML =
+  `
+  <div class="alert-banner">
+    <p><strong>ALERT:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+  </div>
+  `
+
+alertBanner.addEventListener('click', e=>{
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+  }
+});
+
+//MESSAGE FIELD CODE
+send.addEventListener('click', () => {
+  //ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+  } else {
+    alert("Messaage successfully sent to: ${user.value}");
   }
 });
